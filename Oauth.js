19c511dd36aee2a0cc7fdf32f1755386c4f1d0e3
Alan Liu <AlanL.Liu@yahoo.com>
2014-11-01 01:32:01 -0400

    Added items to users collection and added a way to populate them

--- Oauth.js
+++ Oauth.js
@@ -30,6 +30,24 @@ Template.hello.user = function () {
 }
 
 
+Template.hello.buddyEmailDefault =function(){
+  return Meteor.user().buddyEmail==""; 
+}
+
+
+Template.hello.yoUsernameDefault =function(){
+  return Meteor.user().yoUsername==""; 
+}
+
+Template.hello.phoneNumDefault =function(){
+  return Meteor.user().phoneNum==""; 
+}
+
+Template.hello.yoUsernameDefault =function(){
+  return Meteor.user().yoUsername==""; 
+}
+
+
 
 
   Template.hello.events({
@@ -37,18 +55,46 @@ Template.hello.user = function () {
   'click input.code': function () {
     Meteor.call('click');
   },
-
-      'click input.add-property' : function(event){
+      'click input.add-property' : function(event){// add a property (base code to copy paste)
         event.preventDefault();
         var propertyText = document.getElementById("propertyText").value;
-        Meteor.call("addProperty",propertyText,function(error , propertyId){
+        Meteor.call("addProperty",propertyText,function(error , propertyText){
           console.log('added property with text of .. '+propertyText);
         });
         //document.getElementById("propertyText").value = "";
- 
     },
 
-    'click input': function () {
+      'click input.add-buddy-email' : function(event){// maybe add a check for the @?
+        event.preventDefault();
+        var propertyText = document.getElementById("buddyEmail").value;
+        Meteor.call("addBuddyEmail",propertyText,function(error , propertyText){
+          console.log('added buddyEmail with text of '+ propertyText);
+        });
+        //document.getElementById("propertyText").value = "";
+    },
+
+
+      'click input.add-yo-username' : function(event){
+        event.preventDefault();
+        var propertyText = document.getElementById("yoUsername").value;
+        Meteor.call("addYoUsername",propertyText,function(error , propertyText){
+          console.log('added yoUsername with text of  '+propertyText);
+        });
+        //document.getElementById("propertyText").value = "";
+    },
+
+      'click input.add-phone-num' : function(event){
+        event.preventDefault();
+        var propertyText = document.getElementById("phoneNum").value;
+        Meteor.call("addPhoneNum",propertyText,function(error , propertyText){
+          console.log('added phoneNum with text of  '+propertyText);
+        });
+        //document.getElementById("propertyText").value = "";
+    },
+
+
+
+    'click input': function () {//Hello World Code
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
@@ -96,10 +142,19 @@ ServiceConfiguration.configurations.insert({
       user.profile=options.profile;
       user.username=options.profile.name;
     }
-  console.log(options);
-  console.log(user);
-  user.data1 = 0;
-  user.data2 = 0;
+  // console.log(options);
+  // console.log(user);
+
+  user.data1 = 0;//test valu
+  user.yoUsername="";//user's username for Yo! 
+  user.buddyEmail = ""; // acc. buddy email
+  user.phoneNum="";// phone number
+  user.cAvatar=0;// current avatar( 1 2 or 3 )
+  user.wellness=50;//wellness ( from 50 to 100)
+  user.pastAvatars=[];//past avatars
+  user.cAttitude=0;// current attitude 
+  user.avatarStage=0;// avatar's stage (from 0 to 3)
+  console.log(user); // test to make sure all of that was stored
   return user;
 })
 
@@ -111,18 +166,22 @@ ServiceConfiguration.configurations.insert({
 
     Meteor.methods({
   addProperty : function(propertyText){
-    console.log('Adding Property');
-    console.log(Meteor.user());
-
+    console.log('Setting Property to'+ propertyText);
     Meteor.users.update({_id:Meteor.user()._id}, {$set:{"property":propertyText}})
-    // var propertyId= Meteor.users.insert({'property':propertyText});
-    // var propertyId =  Meteor.users.insert({
-    //       'property' : propertyText,
-    //       // 'submittedOn': new Date(),
-    //       // 'submittedBy' : Meteor.userId()
-    //   });
-    console.log(Meteor.user());
-    // return propertyId;
-  }
+  },
+
+  addBuddyEmail : function(propertyText){
+    console.log('Setting Buddy Email to'+ propertyText);
+    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"buddyEmail":propertyText}})
+  },
+  addYoUsername : function(propertyText){
+    console.log('Setting Yo Username to'+ propertyText);
+    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"yoUsername":propertyText}})
+  },
+  addPhoneNum : function(propertyText){
+    console.log('Setting Phone Number to'+ propertyText);
+    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"phoneNum":propertyText}})
+  },
+
 });
 }

eeccd2f33bd113b018b4d606fcb83d6ce612f97d
Summer Wu <sw5813@gmail.com>
2014-11-01 01:15:57 -0400

    create yoUser function

--- routes.js
+++ routes.js
@@ -1,4 +1,19 @@
-Router.map(function(){
-    // routes go here
-    this.route('test');
+Router.route('/', function () {
+  this.render('home');
+});
+
+Router.route('/test');
+
+// given a url like "/yo/sumwoohoo"
+Router.route('/yo/:_username', function () {
+  var params = this.params; // { _username: "sumwoohoo" }
+  var username = params._username; // "sumwoohoo"
+  console.log(username);
+});
+
+// given a url like "/1"
+Router.route('/:_userid', function () {
+  var params = this.params; // { _userid: "1" }
+  var userid = params._userid; // "1"
+  console.log(userid);
 });
